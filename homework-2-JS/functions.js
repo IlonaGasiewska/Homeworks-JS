649c07d6e94894b5410b410bac6a8f0f37203234
ilona <ilona.gasiewska.ilona@gmail.com>
2023-10-29 19:16:54 +0100

    functions

--- homework-2-JS/functions.js
+++ homework-2-JS/functions.js
@@ -1,156 +1,152 @@
-// // 1.	Create a function that returns number of days till Friday
-// function showDaysTillFriday (){
-//   let today = new Date();
-//   let dayOfWeek = today.getDay();
-//   let daysUntilFriday = (5 - dayOfWeek + 7) % 7; 
-//   return daysUntilFriday;
-// };
+// 1.	Create a function that returns number of days till Friday
+function showDaysTillFriday (){
+  let today = new Date();
+  let dayOfWeek = today.getDay();
+  let daysUntilFriday = (5 - dayOfWeek + 7) % 7; 
+  return daysUntilFriday;
+};
 
-// console.log(showDaysTillFriday());
+console.log(showDaysTillFriday());
 
 
   
-// // 2.	Create two functions:
-// // a.	First that takes in a string and shift number, and returns encrypted string using Caesar Cipher
+// 2.	Create two functions:
+// a.	First that takes in a string and shift number, and returns encrypted string using Caesar Cipher
 
-// let alphabet = Array.from("abcdefghijklmnopqrstuvwxyz");
-// const alphabetLength = alphabet.length;
+let alphabet = Array.from("abcdefghijklmnopqrstuvwxyz");
+const alphabetLength = alphabet.length;
 
-// let text = "This is a string to cipher.";
+let text = "This is a string to cipher.";
 
-// function caesarCipher(text, shiftNum) {  
-//   let cipherText = "";
+function caesarCipher(text, shiftNum) {  
+  let cipherText = "";
 
-//   for (let char of text) {
-//     if (char.match(/[a-z]/i)) {
-//       let index = alphabet.indexOf(char.toLowerCase()); 
-//       let newIndex = (index + shiftNum + alphabetLength) % alphabetLength;
+  for (let char of text) {
+    if (char.match(/[a-z]/i)) {
+      let index = alphabet.indexOf(char.toLowerCase()); 
+      let newIndex = (index + shiftNum + alphabetLength) % alphabetLength;
 
-//       if (char === char.toUpperCase()) {
-//         cipherText += alphabet[newIndex].toUpperCase();
-//       } else {
-//         cipherText += alphabet[newIndex];
-//       };
-//     } else {
-//       cipherText += char;
-//     };
-//   };
+      if (char === char.toUpperCase()) {
+        cipherText += alphabet[newIndex].toUpperCase();
+      } else {
+        cipherText += alphabet[newIndex];
+      };
+    } else {
+      cipherText += char;
+    };
+  };
 
-//   return cipherText;
-// };
+  return cipherText;
+};
 
-// console.log(caesarCipher(text, 20));
+console.log(caesarCipher(text, 20));
 
 
 
-// // b.	Second that takes in encrypted string and shift number, and returns decrypted message using Caesar Cipher
+// b.	Second that takes in encrypted string and shift number, and returns decrypted message using Caesar Cipher
 
-// let textToDecrypted = "Nbcm cm u mnlcha ni xymwlsjnyx.";
+let textToDecrypted = "Nbcm cm u mnlcha ni xymwlsjnyx.";
 
-// function caesarDecrypted (text, shiftNum) {
-//   let decryptedText = "";
+function caesarDecrypted (text, shiftNum) {
+  let decryptedText = "";
   
-//     for (let char of text) {
-//       if (char.match(/[a-z]/i)) {
-//         let index = alphabet.indexOf(char.toLowerCase());
-//         let newIndex = (index - shiftNum + alphabetLength) % alphabetLength;
+    for (let char of text) {
+      if (char.match(/[a-z]/i)) {
+        let index = alphabet.indexOf(char.toLowerCase());
+        let newIndex = (index - shiftNum + alphabetLength) % alphabetLength;
 
-//         if(char === char.toUpperCase()){
-//           decryptedText += alphabet[newIndex].toLocaleUpperCase();  
-//         } else {
-//           decryptedText += alphabet[newIndex];
-//         };
+        if(char === char.toUpperCase()){
+          decryptedText += alphabet[newIndex].toLocaleUpperCase();  
+        } else {
+          decryptedText += alphabet[newIndex];
+        };
 
-//       } else {
-//         decryptedText += char;
-//       }
-//     };
+      } else {
+        decryptedText += char;
+      }
+    };
 
-//   return decryptedText;
+  return decryptedText;
 
-// };
+};
 
-// console.log(caesarDecrypted(textToDecrypted, 20));
+console.log(caesarDecrypted(textToDecrypted, 20));
 
   
 // 3.	Create a function that takes in a n (number) as a parameter and returns first n Fibonacci numbers - use recursion
 
 function getFibonacci (n){
-  let fibonacciNumbers = [];
-
   if (n === 0 || n === 1){
-    fibonacciNumbers.push(n);
+    return n;
   } else {
     return getFibonacci(n - 1) + getFibonacci(n - 2);
   };
-
-  return fibonacciNumbers;
 };
 
-console.log(getFibonacci(1));
+console.log(getFibonacci(10));
 
 // Fibonacci in otherway
 
-// function fibonacci (num) {
-//   let fibonacciNumbers = [];
+function fibonacci (num) {
+  let fibonacciNumbers = [];
   
-//   for(let i = 0; i <= num; i++){
-//     i <= 1  &&   i >= 0 ? fibonacciNumbers.push(i) : fibonacciNumbers.push(fibonacciNumbers[fibonacciNumbers.length - 1] + fibonacciNumbers[fibonacciNumbers.length - 2]);
-//   };
+  for(let i = 0; i <= num; i++){
+    i <= 1  &&   i >= 0 ? fibonacciNumbers.push(i) : fibonacciNumbers.push(fibonacciNumbers[fibonacciNumbers.length - 1] + fibonacciNumbers[fibonacciNumbers.length - 2]);
+  };
 
-//   return fibonacciNumbers;
-// };
+  return fibonacciNumbers[fibonacciNumbers.length - 1];
+};
 
-// console.log(fibonacci(10));
+console.log(fibonacci(10));
 
 
-// // 4.	Create a function that:
-// // a.	Checks if a given number is a prime number
+//  4.	Create a function that:
+//  a.	Checks if a given number is a prime number
 
-// function checkNumber(num) {
-//   if (num < 2) {
-//     return `Liczba ${num} nie jest liczbą pierwszą`;
-//   }
+function checkNumber(num) {
+  if (num < 2) {
+    return `Liczba ${num} nie jest liczbą pierwszą`;
+  }
 
-//   if (num === 2) {
-//     return `Liczba ${num} jest liczbą pierwszą!`;
-//   }
+  if (num === 2) {
+    return `Liczba ${num} jest liczbą pierwszą!`;
+  }
 
-//   for (let i = 2; i <= Math.sqrt(num); i++) {
-//     if (num % i === 0) {
-//       return `Liczba ${num} nie jest liczbą pierwszą`;
-//     }
-//   }
-//   return `Liczba ${num} jest liczbą pierwszą!`;
-// }
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return `Liczba ${num} nie jest liczbą pierwszą`;
+    }
+  }
+  return `Liczba ${num} jest liczbą pierwszą!`;
+}
 
-// console.log(checkNumber(1));
+console.log(checkNumber(1));
 
 // // b.	Takes in n (numbers) as a parameter and returns first n prime numbers
 
 
-// function getPrimeNumbers(num) {
-//   let primeNumbers = [];
-//   let numToCheck = 2;
+function getPrimeNumbers(num) {
+  let primeNumbers = [];
+  let numToCheck = 2;
 
-//   while (primeNumbers.length < num) {
-//     let isPrime = true;
+  while (primeNumbers.length < num) {
+    let isPrime = true;
 
-//     for (let i = 2; i <= Math.sqrt(numToCheck); i++) {
-//       if (numToCheck % i === 0) {
-//         isPrime = false;
-//         break;
-//       }
-//     }
+    for (let i = 2; i <= Math.sqrt(numToCheck); i++) {
+      if (numToCheck % i === 0) {
+        isPrime = false;
+        break;
+      }
+    }
 
-//     if (isPrime) {
-//       primeNumbers.push(numToCheck);
-//     }
+    if (isPrime) {
+      primeNumbers.push(numToCheck);
+    }
 
-//     numToCheck++;
-//   }
+    numToCheck++;
+  }
 
-//   return primeNumbers;
-// }
+  return primeNumbers;
+}
 
-// console.log(getPrimeNumbers(5));
+console.log(getPrimeNumbers(5));
